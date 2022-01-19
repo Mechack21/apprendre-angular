a4cf8682bc7d60c18c4af9c931e4fb36c0861de3
Mechack21 <mechackmutokabulambo@gmail.com>
2022-01-19 19:59:19 +0100

    Utilisation des directives part1

--- src/app/appareil/appareil.component.ts
+++ src/app/appareil/appareil.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-appareil',
@@ -7,8 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AppareilComponent implements OnInit {
 
-  appareilName = 'Machine à laver';
-  appareilStatus = 'eteint';
+  @Input() appareilName: string;
+
+  @Input() appareilStatus: string;
 
   constructor() { }
 
